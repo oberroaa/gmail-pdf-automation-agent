c80814e19bb017bc5d9602aeb52f2a2328185f97
Otoniel Berroa Arias <36708192+oberroaa@users.noreply.github.com>
2025-12-30 19:22:06 -0500

    Update agent.js

--- agent.js
+++ agent.js
@@ -1,3 +1,5 @@
+
+
 // ================================
 // TUUCI Gmail PDF Agent
 // ================================
@@ -7,6 +9,14 @@ import { google } from "googleapis";
 import dotenv from "dotenv";
 import analyzePdf from "./analyze-pdf.js";
 import sendWhatsApp from "./whatsapp.js";
+
+console.log("ENV CHECK:", {
+  CLIENT_ID: !!process.env.CLIENT_ID,
+  CLIENT_SECRET: !!process.env.CLIENT_SECRET,
+  REDIRECT_URI: !!process.env.REDIRECT_URI,
+  REFRESH_TOKEN: !!process.env.REFRESH_TOKEN,
+});
+
 
 dotenv.config();
 
