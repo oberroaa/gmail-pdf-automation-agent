237688c412a3c765c538c79555b127b7f1708707
Otoniel Berroa <oberroaa@gmail.com>
2026-04-03 20:52:02 -0400

    refactor: remove obsolete test scripts and debug utilities

--- setup-pdf.js
+++
@@ -1,6 +0,0 @@
-import { DOMMatrix, ImageData, Path2D } from '@napi-rs/canvas';
-
-// Inyectar en el global para que pdfjs-dist los encuentre
-globalThis.DOMMatrix = DOMMatrix;
-globalThis.ImageData = ImageData;
-globalThis.Path2D = Path2D;
